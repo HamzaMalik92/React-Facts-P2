43a4b7f5fa07efe65144aa7c746b57917de78bfe
Hamza Mali <hamzamalikcs20@gmail.com>
2022-04-18 23:08:43 +0500

    first commit

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <h1>Fun Facts about react</h1>
+
+      <ul>
+        <li>Was first release in 2013</li>
+        <li>Build front-end web interfaces</li>
+        <li>React is not a framework</li>
+        <li>React has a strong community</li>
+      </ul>
+    </div>
+  );
+}
